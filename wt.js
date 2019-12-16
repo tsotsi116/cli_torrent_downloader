2dbbc3b2ced0a5f0ef9a578889df00e590eb7aa3
Joshua <6791464+tsotsi116@users.noreply.github.com>
2019-12-16 11:23:53 +0200

    added main script

---
+++ wt.js
@@ -0,0 +1,19 @@
+const WebTorrent = require('webtorrent');
+
+const client = new WebTorrent();
+
+// Put magnet link here
+const mag = '';
+
+client.add(mag, {path: process.cwd()}, (t) => {
+	t.on('done', () => {
+		console.log("I'm Done");
+		process.exit();
+	});
+	console.log(t.name);
+	setInterval(()=> {
+		console.log("Peers: " + t.numPeers);
+		console.log("Progress " + Math.round(t.progress * 100 * 100)/ 100);
+		console.log("Wait for next Update");
+	}, 20000);
+})
